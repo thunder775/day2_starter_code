8e9ddc7225a901df418760eb78ea1792c28d04fb
rahul masih <rahulmasih61@gmail.com>
2019-10-01 10:53:54 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  Container container1 = Container(
+    margin: EdgeInsets.all(2),
+    width: double.infinity,
+    height: 100,
+    color: Colors.blue,
+  );
+  Container container2 = Container(
+    child: Image(
+      image: NetworkImage(
+          'https://mythologian.net/wp-content/uploads/2018/05/Star-of-David-Jewish-Star-Meaning-and-Definition.png'),
+    ),
+    margin: EdgeInsets.all(2),
+    width: double.infinity,
+    height: 100,
+    color: Colors.white,
+  );
+  Container container3 = Container(
+    width: double.infinity,
+    margin: EdgeInsets.all(2),
+    height: 100,
+    color: Colors.blue,
+  );
+  List<Widget> containers = [container1, container2, container3];
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.amberAccent,
-        body: Container(),
+        backgroundColor: Colors.black,
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: containers,
+        )),
       ),
     ),
   );
